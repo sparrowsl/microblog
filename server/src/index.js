63d3382892b0b218e86b32640888fac6ade4e105
sparrowsl <benthorpe.dev@gmail.com>
2024-06-19 13:22:08 +0000

    chore: add zod

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,8 @@
 import { serve } from "@hono/node-server";
 import app from "./app.js";
+import config from "../config/index.js";
 
-serve({ fetch: app.fetch, port: process.env.PORT || 5000 });
+serve({
+	fetch: app.fetch,
+	port: config.PORT,
+});
