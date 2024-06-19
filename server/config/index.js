7194954f3eba45b1d68807e915a44886b14eb4b8
sparrowsl <benthorpe.dev@gmail.com>
2024-06-19 13:22:27 +0000

    add config folder to read from process.env

---
+++ server/config/index.js
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+const config_schema = z.object({
+	PORT: z.number({ coerce: true, message: "PORT is missing" }).default(5000),
+});
+
+function check_config() {
+	const { success, data, error } = config_schema.safeParse(process.env);
+
+	if (!success) {
+		const errors = Object.values(error.flatten().fieldErrors).flat();
+		// biome-ignore lint/suspicious/noConsoleLog: <explanation>
+		console.log("ERROR:", errors[0]);
+		process.exit();
+	}
+
+	return data;
+}
+
+export default check_config();
