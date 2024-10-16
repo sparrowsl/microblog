0c3e4fec7c73d693ee4ea81b64d21094c639924f
sparrowsl <benthorpe.dev@gmail.com>
2024-10-16 12:37:47 +0000

    fix: tests on ci build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
   test: {
-    include: ["src/**/*.{test,spec}.{js,ts}"],
+    // include: ["src/**/*.{test,spec}.{js,ts}"],
   },
 });
