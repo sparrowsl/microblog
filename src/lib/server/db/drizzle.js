13ee00075faa5135dff28e86370d7f44e09015f4
sparrowsl <benthorpe.dev@gmail.com>
2024-10-16 10:47:26 +0000

    chore: upgrade to drizzle 0.35

--- src/lib/server/db/drizzle.js
+++ src/lib/server/db/drizzle.js
@@ -1,8 +1,6 @@
-import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 
 import { config } from "../config.js";
 import * as schema from "./schema.js";
 
-const database = new Database(config.DATABASE_URL);
-export const db = drizzle(database, { schema });
+export const db = drizzle(config.DATABASE_URL, { schema });
